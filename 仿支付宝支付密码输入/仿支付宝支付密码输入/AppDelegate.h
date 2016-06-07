a53701f29ff91f86fa373837b703596413b71b99
yuewenyan <930101433@qq.com>
2016-06-07 10:11:10 +0800

    Update AppDelegate.h
    ---

--- 仿支付宝支付密码输入/仿支付宝支付密码输入/AppDelegate.h
+++ 仿支付宝支付密码输入/仿支付宝支付密码输入/AppDelegate.h
@@ -2,8 +2,8 @@
 //  AppDelegate.h
 //  仿支付宝支付密码输入
 //
-//  Created by CaiMao－yyw on 16/5/30.
-//  Copyright © 2016年 CaiMao－yyw. All rights reserved.
+//  Created by yanyuewen on 16/5/30.
+//  Copyright © 2016年 yanyuewen. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
